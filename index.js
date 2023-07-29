f7d2355e32862b70c44fd5c0dd74865adec4c8db
Snout813 <21900467@handong.ac.kr>
2023-07-29 16:25:50 +0900

    login

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+window.addEventListener("load", function () {
+  const $topBtn = document.querySelector(".moveTopBtn");
+  $topBtn.addEventListener("click", scrollToTop);
+  setTimeout(typing, 1500);
+
+});
+// 버튼을 클릭했을 때 상단으로 이동하는 함수
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth" // 부드러운 스크롤을 위해 "smooth" 속성 사용
+  });
+}
 /* count 숫자 올라가게 만들기 */
 const counter = ($counter, max) => {
     let now = max;
