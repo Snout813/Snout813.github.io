af2f35d9b47fd4397b6ee286de6f812fda929dba
Snout813 <21900467@handong.ac.kr>
2023-07-30 15:57:53 +0900

    update

--- project.js
+++ project.js
@@ -11,25 +11,3 @@ function scrollToTop() {
     behavior: "smooth" // 부드러운 스크롤을 위해 "smooth" 속성 사용
   });
 }
-
-// 카테고리 필터용
-document.querySelectorAll('.button').forEach(button => button.innerHTML = '<div><span>' + button.textContent.trim().split('').join('</span><span>') + '</span></div>');
-
-const categoryBtn = document.querySelector(".menu");
-const projects = document.querySelectorAll(".card");
-
-categoryBtn.onclick("click",(e)=> {
-    const filter = e.target.dataset.filter;
-    if (filter == null){
-        return;
-    }
-    projects.forEach((project)=> {
-        if(filter==="*" || filter===project.dataset.type){
-            project.classList.remove("invisible");
-        }
-        else{
-            project.classList.add("invisible");
-        }
-    })
-})
-
